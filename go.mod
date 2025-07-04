96fef285b29b67633e185cf50ff16b80221a676e
shadmanadman <adman.shadman@gmail.com>
2025-07-04 18:13:34 +0330

    use sqlite instead

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.4
 require (
 	filippo.io/age v1.2.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
--- internal/store.go
+++ internal/store.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"bytes"
+	"database/sql"
+	"errors"
 	"io"
 	"os"
 	"path/filepath"
@@ -16,15 +18,41 @@ func SecretsDir() string {
 	return dir
 }
 
+func dbPath() string {
+	return filepath.Join(SecretsDir(), "secrets.db")
+}
+
+func getDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbPath())
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS secrets(
+	name TEXT PRIMARY KEY,
+	value BLOB
+	);`)
+
+	return db, err
+}
+
 func StoreSecret(name, plaintext, passphrase string) error {
+	db, err := getDB()
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
 	r, err := age.NewScryptRecipient(strings.TrimSpace(passphrase))
+
 	if err != nil {
 		return err
 	}
 
 	var out bytes.Buffer
 	w, err := age.Encrypt(&out, r)
-
 	if err != nil {
 		return err
 	}
@@ -34,30 +62,63 @@ func StoreSecret(name, plaintext, passphrase string) error {
 		return err
 	}
 
+	_, err = io.WriteString(w, plaintext)
+	if err != nil {
+		return err
+	}
 	w.Close()
 
-	path := filepath.Join(SecretsDir(), name+".secret")
-	return os.WriteFile(path, out.Bytes(), 0600)
+	_, err = db.Exec(`
+	INSERT OR REPLACE INTO secrets(name,value) 	VALUES(?,?)`, name, out.Bytes())
+
+	return err
 }
 
 func RetrieveSecret(name, passphrase string) (string, error) {
-	path := filepath.Join(SecretsDir(), name+".secret")
-	data, err := os.ReadFile(path)
+	db, err := getDB()
 	if err != nil {
 		return "", err
 	}
 
-	d, err := age.NewScryptIdentity(strings.TrimSpace(passphrase))
+	defer db.Close()
 
+	var data []byte
+	err = db.QueryRow(`SELECT value FROM secrets WHERE name =?`, name).Scan(&data)
+	if err != nil {
+		return "", errors.New("secret not found")
+	}
+
+	id, err := age.NewScryptIdentity(strings.TrimSpace(passphrase))
 	if err != nil {
 		return "", err
 	}
 
-	r, err := age.Decrypt(bytes.NewReader(data), d)
+	r, err := age.Decrypt(bytes.NewReader(data), id)
 	if err != nil {
 		return "", err
 	}
 
-	out, err := io.ReadAll(r)
-	return string(out), err
+	decrypted, err := io.ReadAll(r)
+	return string(decrypted), err
+}
+
+func DeleteSecret(name string) error {
+	db, err := getDB()
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	res, err := db.Exec(`DELETE FROM secrets WHERE name=?`, name)
+	if err != nil {
+		return err
+	}
+
+	count, _ := res.RowsAffected()
+	if count == 0 {
+		return errors.New("secret not found")
+	}
+
+	return nil
 }
